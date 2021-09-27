e6377aaff9ebc4df93e70395d3027a0c78a79e5b
Priyam <priyamroy29@gmail.com>
2021-09-27 23:57:06 +0530

    initial setup for navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
 	<div class="min-h-full grid grid-layout">
+		<Navbar />
 		<div id="container" class="grid grid-layout-container lg:custom-scroll">
 			<div class="bg-white dark:bg-[#1d1d1d]">
 				<router-view />
 			</div>
+			<!-- <Footer /> -->
 		</div>
 	</div>
 </template>
+
+<script setup lang="ts">
+import Navbar from '~/components/Navbar.vue';
+</script>
 
 <style>
 .grid-layout {
