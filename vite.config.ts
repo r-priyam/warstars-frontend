436cebb9b65a6c346ce08ac0cc5467ef5c2b567d
Priyam <priyamroy29@gmail.com>
2021-09-27 15:05:23 +0530

    use unplugin icons instead of heroicons

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import ViteComponents from 'vite-plugin-components';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 
 export default defineConfig({
 	resolve: {
@@ -8,7 +11,17 @@ export default defineConfig({
 			'~/': `${resolve(__dirname, 'src')}/`,
 		},
 	},
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		ViteComponents({
+			customComponentResolvers: [
+				IconsResolver({
+					componentPrefix: '',
+				}),
+			],
+		}),
+		Icons(),
+	],
 	server: {
 		host: true,
 		open: false,
