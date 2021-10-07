6a6eacaebbe0604bb1b18f10f1eb92ce6963ac87
Priyam <priyamroy29@gmail.com>
2021-10-07 20:09:10 +0530

    fix type issues temporirly

--- src/api/userProfile.ts
+++ src/api/userProfile.ts
@@ -1,15 +1,6 @@
 import axios from 'axios';
 import { apiUrl } from '~/env';
-import {
-	clanAdd,
-	clanAdded,
-	clanRemove,
-	clanRemoved,
-	playerAdd,
-	playerAdded,
-	playerRemove,
-	playerRemoved,
-} from '~/interfaces/userProfile';
+import { clanAddRemove, playerAddRemove } from '~/interfaces/userProfile';
 import { authHeaders } from '~/utils/authHeader';
 
 export const userProfile = {
@@ -25,19 +16,19 @@ export const userProfile = {
 		return axios.get(`${apiUrl}/api/v1/profile/clans`, authHeaders(authToken));
 	},
 
-	async addClan(data: clanAdd, authToken: string) {
-		return axios.post<clanAdded>(`${apiUrl}/api/v1/profile/link-clan`, data, authHeaders(authToken));
+	async addClan(data: clanAddRemove, authToken: string) {
+		return axios.post<clanAddRemove>(`${apiUrl}/api/v1/profile/link-clan`, data, authHeaders(authToken));
 	},
 
-	async removeClan(data: clanRemove, authToken: string) {
-		return axios.post<clanRemoved>(`${apiUrl}/api/v1/profile/unlink-clan`, data, authHeaders(authToken));
+	async removeClan(data: clanAddRemove, authToken: string) {
+		return axios.post<clanAddRemove>(`${apiUrl}/api/v1/profile/unlink-clan`, data, authHeaders(authToken));
 	},
 
-	async addPlayer(data: playerAdd, authToken: string) {
-		return axios.post<playerAdded>(`${apiUrl}/api/v1/profile/link-player`, data, authHeaders(authToken));
+	async addPlayer(data: playerAddRemove, authToken: string) {
+		return axios.post<playerAddRemove>(`${apiUrl}/api/v1/profile/link-player`, data, authHeaders(authToken));
 	},
 
-	async removePlayer(data: playerRemove, authToken: string) {
-		return axios.post<playerRemoved>(`${apiUrl}/api/v1/profile/unlink-player`, data, authHeaders(authToken));
+	async removePlayer(data: playerAddRemove, authToken: string) {
+		return axios.post<playerAddRemove>(`${apiUrl}/api/v1/profile/unlink-player`, data, authHeaders(authToken));
 	},
 };
--- src/interfaces/userProfile.ts
+++ src/interfaces/userProfile.ts
@@ -1,31 +1,11 @@
-export interface clanAdd {
-	clan_tag: string;
+export interface clanAddRemove {
+	clan_name?: string;
+	clan_tag?: string;
+	clan_badge?: string;
 }
 
-export interface clanRemove {
-	clan_tag: string;
+export interface playerAddRemove {
+	player_name?: string;
+	player_tag?: string;
+	town_hall?: string;
 }
-
-export interface playerAdd {
-	player_tag: string;
-	token: string;
-}
-
-export interface playerRemove {
-	player_tag: string;
-}
-
-export interface clanAdded {
-	clan_name: string;
-	clan_tag: string;
-	clan_badge: string;
-}
-
-export interface playerAdded {
-	player_name: string;
-	player_tag: string;
-	town_hall: string;
-}
-
-export interface clanRemoved extends clanAdded {}
-export interface playerRemoved extends playerAdded {}
