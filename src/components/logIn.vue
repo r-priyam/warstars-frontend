0b3e105cd37038765e7f353bc61fe06381c823de
Priyam <priyamroy29@gmail.com>
2021-10-14 11:16:45 +0530

    Equal padding

--- src/components/logIn.vue
+++ src/components/logIn.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="px-2 pt-12 pb-6 md:pt-20 md:px-0">
+	<div class="px-2 pt-6 pb-6 md:px-0">
 		<div class="max-w-lg p-8 mx-auto my-10 bg-main-light-530 rounded-lg shadow-xl dark:bg-main-dark-500 md:p-12">
 			<section>
 				<h3 class="text-3xl font-black text-center text-main-textDark-600 dark:text-main-textLight-530">Login</h3>
