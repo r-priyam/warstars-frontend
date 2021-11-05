f255467de889ade95dab2d7aa466606c652a3cd7
priyamroy <priyamroy29@gmail.com>
2021-11-05 23:33:08 +0530

    add progress bar

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import SignIn from '~/components/logIn.vue';
 import Register from '~/components/signUp.vue';
 import Home from '~/pages/HomePage.vue';
+import NProgress from 'nprogress';
 import NotFound from '~/pages/NotFound.vue';
 
-const routes = [
+const routes: RouteRecordRaw[] = [
 	{ path: '/', component: Home },
 	{ path: '/sign-in', component: SignIn, meta: { requiresAuth: true } },
 	{ path: '/register', component: Register },
@@ -14,6 +15,14 @@ const routes = [
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
+});
+
+router.beforeEach(() => {
+	NProgress.start();
+});
+
+router.afterEach(() => {
+	NProgress.done();
 });
 
 export default router;
