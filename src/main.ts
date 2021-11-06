3dabfd717d947c8d6090ba02fe02c7cf1cdd1d0e
priyamroy <priyamroy29@gmail.com>
2021-11-06 23:52:10 +0530

    remove `vuex` and switch to `pinia`

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { MotionPlugin } from '@vueuse/motion';
-import { store } from '~/store/index';
 import App from './App.vue';
 import './index.css';
 import router from './router';
 
 const app = createApp(App);
-app.use(store);
 app.use(router);
 app.use(MotionPlugin);
 app.mount('#app');
