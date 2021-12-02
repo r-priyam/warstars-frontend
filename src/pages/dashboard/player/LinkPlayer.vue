85a35e998279435014fb762ebb69bd9202c890c0
priyam <priyamroy29@gmail.com>
2021-12-02 16:30:20 +0530

    convert the inputs automatically to uppercase

--- src/pages/dashboard/player/LinkPlayer.vue
+++ src/pages/dashboard/player/LinkPlayer.vue
@@ -16,6 +16,7 @@
 							required="true"
 							class="w-full px-3 py-2 mt-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10 sm:text-sm"
 							placeholder="Your Player Tag"
+							oninput="this.value = this.value.toUpperCase()"
 						/>
 					</div>
 					<div>
