4882e7826b8f725c91fbda9fd6fb2c93cf9d7254
Priyam <priyamroy29@gmail.com>
2021-12-18 02:05:34 +0530

    feat: add component for process button

--- src/pages/dashboard/clan/LinkClan.vue
+++ src/pages/dashboard/clan/LinkClan.vue
@@ -19,33 +19,7 @@
 							oninput="this.value = this.value.toUpperCase()"
 						/>
 					</div>
-					<button
-						v-if="!userClan.linkClanProcessing"
-						class="py-2 font-bold text-white transition duration-200 mt-10 border border-transparent rounded shadow-md bg-main-textDark-560 hover:bg-main-textLight-560 dark:bg-main-textLight-560 dark:hover:bg-main-textDark-560 hover:shadow-xl"
-						type="submit"
-					>
-						Proceed
-					</button>
-					<button
-						v-if="userClan.linkClanProcessing"
-						class="inline-flex items-center justify-center py-2 font-bold text-center text-white mt-10 border border-transparent rounded cursor-not-allowed bg-main-textLight-560 dark:bg-main-textDark-560"
-						disabled
-					>
-						<svg
-							class="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-						>
-							<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-							<path
-								class="opacity-75"
-								fill="currentColor"
-								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-							></path>
-						</svg>
-						Processing
-					</button>
+					<ProcessButton :processing="userClan.linkClanProcessing" />
 				</form>
 			</section>
 		</div>
@@ -54,6 +28,8 @@
 
 <script setup lang="ts">
 import { userClan as userClanOperations } from '~/stores/userClan';
+import ProcessButton from '~/components/ProcessButton.vue';
+
 const userClan = userClanOperations();
 
 async function linkClan() {
--- src/pages/dashboard/leagues/season/SeasonClansadd.vue
+++ src/pages/dashboard/leagues/season/SeasonClansadd.vue
@@ -36,33 +36,7 @@
 						>
 						<textarea name="clan-tags" class="register-form-input" rows="1" required></textarea>
 					</div>
-					<button
-						v-if="!league.seasonClanAddProcess"
-						class="py-2 mt-10 font-bold text-white transition duration-200 border border-transparent rounded shadow-md bg-main-textDark-560 hover:bg-main-textLight-560 dark:bg-main-textLight-560 dark:hover:bg-main-textDark-560 hover:shadow-xl"
-						type="submit"
-					>
-						Proceed
-					</button>
-					<button
-						v-if="league.seasonClanAddProcess"
-						class="inline-flex items-center justify-center py-2 font-bold text-center text-white mt-10 border border-transparent rounded cursor-not-allowed bg-main-textLight-560 dark:bg-main-textDark-560"
-						disabled
-					>
-						<svg
-							class="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-						>
-							<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-							<path
-								class="opacity-75"
-								fill="currentColor"
-								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-							></path>
-						</svg>
-						Processing
-					</button>
+					<ProcessButton :processing="league.seasonClanAddProcess" />
 				</form>
 			</section>
 		</div>
@@ -72,6 +46,7 @@
 <script setup lang="ts">
 import PopUp from '~/pages/dashboard/utils/ConfirmationPopup.vue';
 import router from '~/router';
+import ProcessButton from '~/components/ProcessButton.vue';
 import { notifications } from '~/stores/notifications';
 import { leagueManagement } from '~/stores/leagueManagement';
 import { formatTag, isValidTag } from '~/utils/clashUtils';
