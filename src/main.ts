e6a5b7f90baec88bcc6c12bc851844b599915ed4
Priyam <priyamroy29@gmail.com>
2022-02-08 03:38:54 +0530

    more changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import './index.css';
 import router from './router';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
