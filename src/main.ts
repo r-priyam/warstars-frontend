146bd2b8b5194ed6f20386e4465cbb0983bcce94
Priyam <priyamroy29@gmail.com>
2022-02-08 23:41:55 +0530

    fix build failing

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue';
 import './index.css';
 import routes from './router';
 
-export const createApp = ViteSSG(App, { routes, base: import.meta.env.BASE_URL }, (ctx) => {
+export const createApp = ViteSSG(App, { routes, base: import.meta.env.BASE_URL }, async (ctx) => {
+	// https://github.com/antfu/vite-ssg/issues/103
+	if (!ctx.isClient) await ctx.router.isReady();
 	Object.values(import.meta.globEager('./modules/*.ts')).forEach((i) => i.install?.(ctx));
 });
